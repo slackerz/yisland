a5d5be23567c81e243810661883ffe20203b1d81
Gerard Thomas <slacker@slackerzgroup.com>
2013-07-16 22:20:23 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yisland::Application.config.secret_token = '6d3b96ce34bf26a62215fbab74e6c86f2cc2ed59fe81bf805ffafc036a9ce93aa49a98c70f3232d52d706d917b859202ea1f7e41bd173eec549f3654be43d7f8'
